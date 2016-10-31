b0ec249dc058bf3224ceddf80f951099ff13b26f
adrien <adrien.peyrache@gmail.com>
2016-10-31 16:26:05 -0400

    Clean helper

--- KiloSortWrapper.m
+++ KiloSortWrapper.m
@@ -1,18 +1,47 @@
-function master_file(basepath,basename)
-%addpath(genpath('/home/brendon/gitrepositories/KiloSort')) % path to kilosort folder
-%addpath(genpath('/home/brendon/gitrepositories/npy-matlab')) % path to npy-matlab scripts
+function KiloSortWrapper(basepath,basename)
 
-%% BW STUFF
+% Creates channel map from Neuroscope xml files, runs KiloSort and
+% writes output data in the Neuroscope/Klusters format. 
+% StandardConfig.m should be in the path or copied to the local folder
+% 
+%  USAGE
+%
+%    KiloSortWrapper()
+%    Should be run from the data folder, and file basenames are the
+%    same as the name as current directory
+%
+%    KiloSortWrapper(basepath,basenmae)
+%
+%    INPUTS
+%    basepath       path to the folder containing the data
+%    basename       file basenames (of the dat and xml files)
+%
+%    Dependencies:  KiloSort (https://github.com/cortex-lab/KiloSort)
+
+% Copyright (C) 2016 Brendon Watson, Adrien Peyrache
+%
+% This program is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 2 of the License, or
+% (at your option) any later version.
+
+%% Addpath if needed
+%addpath(genpath('gitrepositories/KiloSort')) % path to kilosort folder
+%addpath(genpath('gitrepositories/npy-matlab')) % path to npy-matlab scripts
+
+%% If function is called without argument
 if ~exist('basepath','var')
    [~,basename] = fileparts(cd);
    basepath = cd; 
 end
+
+%% If a channel map does not exist
 if ~exist(fullfile(basepath,'chanMap.mat'))
     createChannelMapFile_Local(basepath)
 end
 
 %% default options are in parenthesis after the comment
-pathToYourConfigFile = basepath; % take from Github folder and put it somewhere else (together with the master_file)
+pathToYourConfigFile = basepath; % you can change this line to another folder
 run(fullfile(pathToYourConfigFile, 'StandardConfig.m'))
 
 tic; % start timer
@@ -24,7 +53,8 @@ end
 if strcmp(ops.datatype , 'openEphys')
    ops = convertOpenEphysToRawBInary(ops);  % convert data, only for OpenEphys
 end
-%%
+
+%% Lauches KiloSort
 
 disp('PreprocessingData')
 [rez, DATA, uproj] = preprocessData(ops); % preprocess data and extract spikes for initialization
@@ -46,10 +76,7 @@ save(fullfile(ops.root,  'rez.mat'), 'rez', '-v7.3');
 % disp('Starting to convert to Phy format')
 % rezToPhy(rez, ops.root);
 disp('Starting to convert to Klusters format')
-keyboard
 ConvertKilosort2Neurosuite(basepath,basename,rez)
 
 %% remove temporary file
 delete(ops.fproc);
-
-%%
